0cdfabcab9a205618f40304c59d6f6e20f903519
panchhipatel <panchhipatel0204@gmail.com>
2026-07-17 12:09:57 +0000

    Fix CSV filename

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Uber Analytics",layout="wide")
 
 @st.cache_data
 def load_data():
-     df = pd.read_csv("Uber.csv")
+     df = pd.read_csv("uber.csv")
      return df
 df = load_data()
 
